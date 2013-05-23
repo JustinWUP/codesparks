c0ee1bf95ed9302d8451c78f034d2e4186c496ea
Justin Bean <Justin@domandtom.com>
2013-05-23 15:51:39 -0500

    adding some helpers

--- application/helpers/jbean_helper.php
+++ application/helpers/jbean_helper.php
@@ -17,3 +17,41 @@ if ( ! function_exists('image_url'))
 		return $CI->config->slash_item('base_url') . 'assets/images/' . $data;
 	}
 }
+
+// ------------------------------------------------------------------------
+
+/**
+ * Script Url
+ *
+ * Returns the "script_url" based on the base_url from the config file
+ *
+ * @access	public
+ * @return	string
+ */
+if ( ! function_exists('script_url'))
+{
+	function script_url($data)
+	{
+		$CI =& get_instance();
+		return $CI->config->slash_item('base_url') . 'assets/js/' . $data;
+	}
+}
+
+// ------------------------------------------------------------------------
+
+/**
+ * CSS Url
+ *
+ * Returns the "CSS_url" based on the base_url from the config file
+ *
+ * @access	public
+ * @return	string
+ */
+if ( ! function_exists('css_url'))
+{
+	function css_url($data)
+	{
+		$CI =& get_instance();
+		return $CI->config->slash_item('base_url') . 'assets/css/' . $data;
+	}
+}
