e8bbfafa2e01e2e39c3cb1628224e6e33ff91586
Justin Bean <Justin@domandtom.com>
2013-05-24 14:24:37 -0500

    added option for compass

--- README.md
+++ README.md
@@ -3,12 +3,18 @@ CodeSparks
 
 CodeSparks is the official J.Bean personal CodeIgniter template.
 
-## TO INSTALL 
+## TO INSTALL VANILLA SPARKS
 Run this at your terminal (where test is the folder to install to and testsite is the domain)
 
     curl -L  https://github.com/JustinWUP/citemplate/raw/master/ci_install.sh | sh -s test testsite
 
+## TO INSTALL WITH COMPASS (SASS)
+Run this at your terminal (where test is the folder to install to and testsite is the domain)
+
+    curl -L  https://github.com/JustinWUP/citemplate/raw/master/ci_install.sh | sh -s test testsite (compass)
+
 Your site will be at ~/Sites/test and online at testsite.spark
 
 ## NOTES
-This assumes you have Apache set up and Vhosts on.
+This assumes you have Apache set up and Vhosts on. 
+Also, Ruby if you're using Compass.
--- ci_install.sh
+++ ci_install.sh
@@ -8,15 +8,19 @@
 argument1=$1  ###This is your incoming argument.
 argument2=$2
 argument3=$(whoami)
+slots=$3
 escapestring=(echo '$1')
 
+echo 'Getting Sparks...'
 mkdir ~/Sites/$argument1 ###This is your command using the argument.
 git clone https://github.com/JustinWUP/citemplate.git  ~/Sites/$argument1 
 
+echo 'Modifying your hosts file for our project...'
 sudo -- sh -c "echo  >> /etc/hosts"
 sudo -- sh -c "echo 127.0.0.1 $argument2.spark >> /etc/hosts"
 sudo -- sh -c "echo 127.0.0.1 www.$argument2.spark >> /etc/hosts"
 
+echo 'Modifying your Apache root .htaccess...'
 sudo -- sh -c "echo  >> ~/Sites/.htaccess"
 sudo -- sh -c "echo \#for site $argument2\.spark >> ~/Sites/.htaccess"
 sudo -- sh -c "echo RewriteEngine On  >> ~/Sites/.htaccess"
@@ -24,7 +28,7 @@ sudo -- sh -c "echo RewriteCond %{HTTP_HOST} ^\(www\\\.\)?$argument2\\\.spark$ [
 sudo -- sh -c "echo RewriteCond %{REQUEST_URI} !^/~$argument3/$argument1 >> ~/Sites/.htaccess"
 sudo -- sh -c "echo RewriteRule ^\(.*\)$ /~$argument3/$argument1/\$\1 [L] >> ~/Sites/.htaccess"
 
-
+echo 'Creating an .htaccess file for your project...'
 sudo -- sh -c "echo  >> ~/Sites/$argument1/.htaccess"
 sudo -- sh -c "echo \<IfModule mod_rewrite.c\> >> ~/Sites/$argument1/.htaccess"
 sudo -- sh -c "echo     Options +FollowSymLinks >> ~/Sites/$argument1/.htaccess"
@@ -37,7 +41,7 @@ sudo -- sh -c "echo \</IfModule\>  >> ~/Sites/$argument1/.htaccess"
 rm ~/Sites/$argument1/ci_install.sh
 
 #escape these arrow brackets
-
+echo 'Setting up a VirtualHost...'
 sudo -- sh -c "echo  >> /private/etc/apache2/extra/httpd-vhosts.conf"
 sudo -- sh -c "echo     \<VirtualHost *:80\> >> /private/etc/apache2/extra/httpd-vhosts.conf"
 sudo -- sh -c "echo ServerName $argument2.spark >> /private/etc/apache2/extra/httpd-vhosts.conf"
@@ -50,7 +54,29 @@ sudo -- sh -c "echo             Allow from All >> /private/etc/apache2/extra/htt
 sudo -- sh -c "echo 		\</Directory\> >> /private/etc/apache2/extra/httpd-vhosts.conf"
 sudo -- sh -c "echo     \</VirtualHost\> >> /private/etc/apache2/extra/httpd-vhosts.conf"
 
+echo 'Restarting Apache...'
 sudo apachectl restart
+
+case "${slots[@]}" in  *"compass"*)
+	echo 'Installing Compass...'  
+	gem install compass 
+	echo 'Setting up your project for Compass...'
+	mkdir ~/Sites/$argument1/assets/sass 
+	sudo -- sh -c "echo http_path = \'/\' >> ~/Sites/$argument1/config.rb"
+	sudo -- sh -c "echo sass_dir = \'assets/sass\'  >> ~/Sites/$argument1/config.rb"                   
+ 	sudo -- sh -c "echo css_dir = \'assets/css\' >> ~/Sites/$argument1/config.rb"
+ 	sudo -- sh -c "echo images_dir = \'assets/images\' >> ~/Sites/$argument1/config.rb"
+ 	sudo -- sh -c "echo javascripts_dir = \'assets/js\' >> ~/Sites/$argument1/config.rb"
+  	sudo -- sh -c "echo http_stylesheets_path = \'assets/css\' >> ~/Sites/$argument1/config.rb"
+  	sudo -- sh -c "echo http_javascripts_path = \'assets/js\' >> ~/Sites/$argument1/config.rb"
+  	sudo -- sh -c "echo http_images_path = \'assets/images\' >> ~/Sites/$argument1/config.rb"
+  	sudo -- sh -c "echo environment = :development >> ~/Sites/$argument1/config.rb"
+  	sudo -- sh -c "echo output_style = :expanded >> ~/Sites/$argument1/config.rb"
+  	sudo -- sh -c "echo relative_assets = true >> ~/Sites/$argument1/config.rb" ;; esac
+
+case "${slots[@]}" in  *"compass"*)
+
+echo 'You sparked it.'
 
 open http://$argument2.spark
 
