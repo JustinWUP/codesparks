7ab8d390185d53db9e2140baa9db2525bc45e226
Justin Bean <Justin@domandtom.com>
2013-05-25 18:54:14 -0500

    revise index.php

--- application/views/sparks/index.php
+++ application/views/sparks/index.php
@@ -2,16 +2,30 @@
 <hr>
 <h3>Tips and some differences from stock CodeIgniter</h3>
 <ul>
-	<li>Set your environment variable in application/config/environment.php and 
+	<li>Set your environment variable in application/config/environment.php and <br>
 		then use the corresponding environment folders in application/config for database and app setup.</li>
 	<li>Assets are in the assets folder and there are url helpers preinstalled to access things there
 		(get info on those in helpers/sparks_helper.php.</li>
-	<li>There are header and footer partials pre-installed that have all the extraneous stuff I use for those things
-		are included to reduce clutter in views.</li>
+	<li>There are header and footer partials pre-installed that have all the extraneous stuff<br>
+	 	I use for those things are included to reduce clutter in views.</li>
 	<li>Your header, footer and other partials get loaded into views by the decidedly more concise function
 		<pre>partial('partialname');</pre> </li>
 	<li>The helper functions are designed to be used with the php echo helper, like so.
 		<pre>&lt;?= link_to('url', image('image.jpg')); ?&gt;</pre></li>
+	<li>Your simplest controller looks like this.
+		<pre> 	
+		class Sparks extends CI_Controller {
+			public function index()
+			{	
+				template();
+			}
+		]
+		</pre>
+		The URL for this is yoursite.com/CLASS_NAME/index.<br>
+		Every page view request hits the specified template (or in this case default template) first and then includes the view based on the classname/methodname convention.<br>
+		The default template is at views/templates/core.php but you can change it or have multiple.
+	</li>
+
 </ul>
 
 <h3>Get Started</h3>
@@ -20,5 +34,3 @@
 	<li>Create yourself a view folder and controller</li>
 	<li>Change default controller in routes.php to the controller you want</li>
 </ol>
-
-
