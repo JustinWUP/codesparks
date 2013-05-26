938acc1dbadc3db2162d405e2427818ea6eeb93b
Justin Bean <Justin@domandtom.com>
2013-05-26 12:12:06 -0500

    making a better scaffold script

--- scaffold.sh
+++ scaffold.sh
@@ -4,7 +4,10 @@
 
 #!/bin/bash
 
-argument1=$1 
+slots=$1
+argument1=$2
+
+function scaffold {
 for i in $argument1; do B=`echo "${i:0:1}" | tr a-z A-Z`${i:1}; done
 
 echo 'Generating scaffold...'
@@ -22,5 +25,23 @@ echo "		template();" >> ./application/controllers/$argument1.php
 echo "    }" >> ./application/controllers/$argument1.php
 echo "}  " >> ./application/controllers/$argument1.php
 echo 'Scaffold created!'
+}
+
+case "${slots[@]}" in  *"setup"*)
+rm ./application/controllers/sparks.php
+rm ./application/views/index.php
+perl -pi -e "s/'sparks'/'$argument1'/g" ./application/config/routes.php
+scaffold
+;; esac
+
+if [ $# -eq 0 ] ; then
+echo "You didn't enter any arguments."
+echo "If this is a new project, run ./scaffold.sh setup {new scaffold name} "
+echo "If you're just generating new views and controllers, run ./scaffold.sh generate {new scaffold name}"
+fi
+
+case "${slots[@]}" in  *"generate"*)
+scaffold
+;; esac
 
 ###END SCRIPT###
