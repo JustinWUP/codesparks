7a3b13365e7da7fc1fc8b346a953b50969079861
Justin Bean <Justin@domandtom.com>
2013-05-25 21:19:41 -0500

    added basic scaffolding tool

---
+++ scaffold.sh
@@ -0,0 +1,23 @@
+###BEGIN SCRIPT###
+
+###SCRIPT NAME:  scaffold.sh ###
+
+#!/bin/bash
+
+argument1=$1 
+for i in $argument1; do B=`echo "${i:0:1}" | tr a-z A-Z`${i:1}; done
+
+echo 'Generating scaffold...'
+mkdir ./application/views/$argument1  
+touch ./application/views/$argument1/index.php
+echo "<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');" >> ./application/controllers/$argument1.php
+echo " "  >> ./application/controllers/$argument1.php
+echo "class $B extends CI_Controller {" >> ./application/controllers/$argument1.php
+echo " " >> ./application/controllers/$argument1.php
+echo "public function index()" >> ./application/controllers/$argument1.php
+echo "	{ " >> ./application/controllers/$argument1.php
+echo "		template();" >> ./application/controllers/$argument1.php
+echo "    }" >> ./application/controllers/$argument1.php
+echo "}  " >> ./application/controllers/$argument1.php
+
+###END SCRIPT###
