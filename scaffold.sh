6701a67df75fc0f60e67e0899e8b4fab21b513a6
Justin Bean <Justin@domandtom.com>
2013-05-25 21:22:59 -0500

    scaffold creates partials folder in views

--- scaffold.sh
+++ scaffold.sh
@@ -9,7 +9,9 @@ for i in $argument1; do B=`echo "${i:0:1}" | tr a-z A-Z`${i:1}; done
 
 echo 'Generating scaffold...'
 mkdir ./application/views/$argument1  
+mkdir ./application/views/$argument1/partials  
 touch ./application/views/$argument1/index.php
+
 echo "<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');" >> ./application/controllers/$argument1.php
 echo " "  >> ./application/controllers/$argument1.php
 echo "class $B extends CI_Controller {" >> ./application/controllers/$argument1.php
